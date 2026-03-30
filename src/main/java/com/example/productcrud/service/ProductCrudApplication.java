e024471368f002644962a4e8bcfabb7b161940b0
jeremynababan <jeremynababan11@gmail.com>
2026-03-30 10:57:43 +0700

    project-Crud

--- src/main/java/com/example/productcrud/service/ProductCrudApplication.java
+++ src/main/java/com/example/productcrud/service/ProductCrudApplication.java
@@ -1,4 +1,4 @@
-package com.example.productcrud;
+package com.example.productcrud.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
