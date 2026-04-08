03a24fa419db9448f14a99318297c8c8bd8d429d
jeremynababan <jeremynababan11@gmail.com>
2026-04-08 23:04:19 +0700

    menambahkan tugas handsone

--- src/main/java/com/example/productcrud/ProductCrudApplication.java
+++ src/main/java/com/example/productcrud/ProductCrudApplication.java
@@ -1,4 +1,4 @@
-package com.example.productcrud.service;
+package com.example.productcrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
